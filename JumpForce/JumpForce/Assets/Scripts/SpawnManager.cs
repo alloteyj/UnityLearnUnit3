c27e88f0a303b6342ecdf9b3702df2b76e0e578d
Jonathan Allotey <alloteyj@roverkids.org>
2026-01-05 13:46:27 -0500

    3.2 Done

--- JumpForce/JumpForce/Assets/Scripts/SpawnManager.cs
+++ JumpForce/JumpForce/Assets/Scripts/SpawnManager.cs
@@ -10,8 +10,8 @@ public class SpawnManager : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        InvokeRepeating("SpawnObstacle", startDelay, repeatRate);
         playercontrollerScript = GameObject.Find("Player").GetComponent<PlayerController>();
+        InvokeRepeating("SpawnObstacle", startDelay, repeatRate);
     }
 
     // Update is called once per frame
